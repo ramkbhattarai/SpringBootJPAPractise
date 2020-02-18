31e85c131fad330f89ace8ad7687b8e154fd2d02
Ram <ramkb466@gmail.com>
2020-02-17 20:21:16 -0500

    tested all the crud operation

--- src/main/java/com/rkb/springBootStrater/topic/TopicService.java
+++ src/main/java/com/rkb/springBootStrater/topic/TopicService.java
@@ -11,12 +11,6 @@ import org.springframework.stereotype.Service;
 public class TopicService {
 	@Autowired
 	private TopicRepository topicRepository;
-	private List<Topic> topics = new ArrayList<>(Arrays.asList(
-			new Topic(1, "spring framework", "spring framework description"),
-			new Topic(2, "java", "java framework description"),
-			new Topic(3, "javascript", "javascript framework description"),
-			new Topic(4, "angular", "angular framework description")
-			));
 	
 	public List<Topic> getAllTopics(){
 		List<Topic> topics = new ArrayList<>();
